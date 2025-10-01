94068461611997daf40d5dbb723c254f8e23674b
wyane-ubuntu <denver.delamasa@gmail.com>
2025-10-01 22:29:23 +0800

    feat: Homepage sections added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,19 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-        <main className="min-h-[100vh]">
+        <main className="min-h-[88vh]">
+          <section className="min-h-[88vh] bg-white border-black">
+
+          </section>
+          <section className="min-h-[100vh] bg-[#1971c2] border-b-2 border-t-4 border-black">
+
+          </section>
+          <section className="min-h-[100vh] bg-white border-b-2 border-t-2 border-black">
+
+          </section>
+          <section className="min-h-[100vh] bg-[#ffd43b] border-b-4 border-t-2 border-black">
+            
+          </section>
         </main>
       <Footer/>
     </>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <footer className="m-2 footer-grid min-h-[40vh] neo-brutalist">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 min-h-[10vh]">
-        <div></div>
-    </div>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 min-h-[10vh]">
+          <div></div>
+      </div>
     </footer>
   );
 }
