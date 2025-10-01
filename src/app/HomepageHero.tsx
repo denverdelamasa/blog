3d948247c9ce875b5f4325674b2215219a5b8c7f
wyane-ubuntu <denver.delamasa@gmail.com>
2025-10-01 23:58:51 +0800

    feat: HomepageHero section typography

---
+++ src/app/HomepageHero.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function HomepageHero() {
+  return (
+    <>
+        <div className="hero-content flex flex-col lg:flex-row justify-evenly text-black w-full h-full">
+            <div className="flex flex-col gap-2">
+                <h1 className="text-[8rem] text-wrap">
+                    Welcome to my Blog!
+                </h1>
+                <p className="text-xl">
+                    ● a place where I jot stuff down.
+                </p>
+            </div>
+            <div className="bg-[#ffd43b] neo-brutalist min-w-[500px] min-h-[600px]">
+                3D spline here
+            </div>
+        </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HomepageHero from "./HomepageHero";
+
 export default function Home() {
   return (
     <>
-      <main className="min-h-[88vh] flex flex-col gap-4">
-        <section className="min-h-[88vh] bg-white">
-
+      <main className="min-h-[88vh] flex flex-col gap-4 my-4">
+        <section className="hero min-h-[88vh] bg-white">
+          <HomepageHero/>
         </section>
         <section className="min-h-[100vh] bg-[#1971c2] neo-brutalist">
 
