4bb44ff6968fab41338117a261ccc519807fc039
wyane-ubuntu <denver.delamasa@gmail.com>
2025-10-02 01:30:47 +0800

    feat: About section flex content

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-        <div className="hero-content text-black w-full h-full justify-start">
+        <div className="hero-content flex flex-col text-black w-full h-full md:justify-start md:items-start">
             <div className="flex flex-row">
                 <div className="my-auto w-[8vh] h-[8vh] m-4 neo-brutalist  flex items-center justify-center">
                     <i className="bi bi-info-circle text-black text-[5vh]"></i>
@@ -11,6 +11,14 @@ export default function About() {
                 <h1 className="text-7xl text-wrap my-auto">
                     About
                 </h1>
+            </div>
+            <div className="flex flex-col md:flex-row w-full h-full gap-4">
+                <div className="neo-brutalist w-full md:w-4/6 h-full">
+                sample
+                </div>
+                <div className="neo-brutalist w-full md:w-2/6 h-full">
+                sample
+                </div>
             </div>
         </div>
     </>
