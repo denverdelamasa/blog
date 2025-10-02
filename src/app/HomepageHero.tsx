41a7b446956d3935b5bd9f3d0686cffb9d80fd93
wyane-ubuntu <denver.delamasa@gmail.com>
2025-10-02 19:09:52 +0800

    style: added border in HomepageHero

--- src/app/HomepageHero.tsx
+++ src/app/HomepageHero.tsx
@@ -4,7 +4,7 @@ import Spline3D from "@/components/Spline3D";
 export default function HomepageHero() {
   return (
     <>
-        <div className="hero-content flex flex-col lg:flex-row justify-evenly text-black w-full h-full">
+        <div className="hero-content flex flex-col lg:flex-row justify-evenly text-black w-full h-full m-4">
             <div className="flex flex-col gap-2">
                 <h1 className="text-6xl md:text-[7rem] break-all md:break-normal">
                     Welcome to my Blog!
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <>
       <main className="min-h-[88vh] flex flex-col gap-4 my-4">
-        <section className="hero min-h-[70vh] md:min-h-[88vh] bg-white">
+        <section className="hero min-h-[70vh] md:min-h-[88vh] border-2 border-black rounded-[8px]">
           <HomepageHero/>
         </section>
         <section className="min-h-[100vh] bg-[#1971c2] rounded-[8px]">
 
         </section>
-        <section className="h-auto bg-white  rounded-[8px] border-4 border-black">
+        <section className="h-auto bg-white border-2 border-black rounded-[8px]">
           <RecentBlogs/>
         </section>
         <section id="about" className="h-auto bg-[#ffd43b] rounded-[8px]">
