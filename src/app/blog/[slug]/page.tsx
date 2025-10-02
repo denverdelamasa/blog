fdf13058f94d7b2297c6d9d53f0706a7123e4964
wyane-ubuntu <denver.delamasa@gmail.com>
2025-10-02 23:09:54 +0800

    fix: [slug] page text

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,11 +17,11 @@ export default async function PostPage(props: Params) {
   return (
     <main className="max-w-3xl mx-auto py-12 px-4">
       <article>
-        <h1 className="text-4xl font-bold mb-2">{post.frontMatter.title}</h1>
-        <p className="text-sm opacity-70 mb-6">{post.frontMatter.date}</p>
+        <h1 className="text-4xl font-bold mb-2 text-black">{post.frontMatter.title}</h1>
+        <p className="text-sm opacity-70 mb-6 text-gray-400">{post.frontMatter.date}</p>
 
         <div
-          className="prose prose-lg dark:prose-invert"
+          className="prose prose-lg dark:prose-invert text-gray-900"
           dangerouslySetInnerHTML={{ __html: post.contentHtml }}
         />
 
