0adcd97b5cdf936614d59ce36c3871dbfe6f386c
wyane-ubuntu <denver.delamasa@gmail.com>
2025-10-03 16:48:02 +0800

    style: tweaked loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,36 +1,15 @@
 import styled from 'styled-components';
 
 const Spinner = styled.div`
-  position: relative;
-  width: 32px;
-  height: 32px;
-
-  &::before,
-  &::after {
-    content: '';
-    width: 100%;
-    height: 100%;
-    display: block;
-    animation: spinner-b4c8mmsm 0.5s backwards, spinner-49opz7sm 1.25s 0.5s infinite ease;
-    border: 4px solid #474bff;
-    border-radius: 50%;
-    box-shadow: 0 -24px 0 -4px #474bff;
-    position: absolute;
-  }
-
-  &::after {
-    animation-delay: 0s, 1.25s;
-  }
-
-  @keyframes spinner-b4c8mmsm {
-    from {
-      box-shadow: 0 0 0 -4px #474bff;
-    }
-  }
-
-  @keyframes spinner-49opz7sm {
+  width: 5.6px;
+  height: 5.6px;
+  animation: spinner-xp626r 1s infinite;
+  border-radius: 50%;
+  box-shadow: 22.4px 0 0 5.6px #323232, -22.4px 0 0 5.6px #323232;
+  
+  @keyframes spinner-xp626r {
     to {
-      transform: rotate(360deg);
+        transform: rotate(360deg);
     }
   }
 `;
